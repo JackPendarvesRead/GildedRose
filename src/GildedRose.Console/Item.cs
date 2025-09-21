6e16211fd73c1a257f59cc32fd2dde47a051447d
JackRead <jackpendarvesread@gmail.com>
2025-09-21 17:42:05 +0100

    Updated .net framework and did some small refactoring to make the code easier to read

---
+++ src/GildedRose.Console/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace GildedRose.Console
+{
+    // NOTE: this class must NOT be modified as it is owned by the goblins.
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
