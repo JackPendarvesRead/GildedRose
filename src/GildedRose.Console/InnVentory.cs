5584d3cc6e102965f912178f250870ca34da24a9
JackRead <jackpendarvesread@gmail.com>
2025-09-21 18:51:38 +0100

    Made InnVentory internal use only as I realised that is would be stupid to use this for anything except the initial setup

--- src/GildedRose.Console/InnVentory.cs
+++ src/GildedRose.Console/InnVentory.cs
@@ -6,15 +6,10 @@ using System.Threading.Tasks;
 
 namespace GildedRose.Console
 {
-    public static class InnVentory
+    // INTERNAL USE ONLY
+    // This is just to make calling the initial set of items easier
+    internal static class InnVentory
     {
-        public static Item DexterityVestPlus5 = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
-        public static Item AgedBrie = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
-        public static Item ElixirOfTheMongoose = new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
-        public static Item SulfurasHandOfRagnaros = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
-        public static Item BackstagePassesToATAFKAL80ETCConcert = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 };
-        public static Item ConjuredManaCake = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 };
-    
         public static IList<Item> GetAllItems()
         {
             return new List<Item>
@@ -27,5 +22,12 @@ namespace GildedRose.Console
                 ConjuredManaCake
             };
         }
+
+        private static Item DexterityVestPlus5 = new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 };
+        private static Item AgedBrie = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+        private static Item ElixirOfTheMongoose = new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
+        private static Item SulfurasHandOfRagnaros = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
+        private static Item BackstagePassesToATAFKAL80ETCConcert = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 };
+        private static Item ConjuredManaCake = new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 };
     }
 }
